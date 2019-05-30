cc8727a8f3a75b6549be7cb8d190c70ec83dae7c
raunakrajvanshi <raunakrajvanshi@gmail.com>
2019-05-30 18:28:21 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,133 @@
+import React , {Component} from 'react';
+import { StyleSheet, Text, View , Button } from 'react-native';
+import {
+  createSwitchNavigator,
+  createAppContainer,
+  createDrawerNavigator,
+  createBottomTabNavigator,
+  createStackNavigator
+} from 'react-navigation';
+import Icon from '@expo/vector-icons/Ionicons'
+
+
+
+  class App extends Component {
+  render() {
+    return <AppContainer />;
+    }
+  }
+  export default App;
+
+class WelcomeScreen extends Component {
+  render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button title="Login"
+        onPress={()=>this.props.navigation.navigate('Dashboard')}
+          />
+        </View>
+        );
+      }
+    }
+
+class DashboardScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>DashboardScreen</Text>
+      </View>
+      );
+    }
+  }
+
+
+class Utilities extends Component {
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Utilities</Text>
+        </View>
+      );
+    }
+  }
+
+
+class Workout extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>WorkoutScreen</Text>
+      </View>
+    );
+  }
+}
+
+
+class Home extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>HomeScreen</Text>
+      </View>
+    );
+  }
+}
+
+
+const DashboardTabNavigator = createBottomTabNavigator(
+  {
+    Home,
+    Workout,
+    Utilities
+  },
+    {
+      navigationOptions: ({ navigation }) => {   //prop passed
+        const { routeName } = navigation.state.routes[navigation.state.index]; // {routename} checks whether it is home workout or utilities
+          return {
+            headerTitle: routeName
+          };
+        }
+      }
+    );
+
+
+const DashboardStackNavigator = createStackNavigator(
+  {
+    DashboardTabNavigator: DashboardTabNavigator
+  },
+    {
+      defaultNavigationOptions:({navigation}) => {
+        return {
+          headerLeft: < Icon style={{ paddingLeft: 10 }}
+          onPress={() => navigation.openDrawer()}
+          name="ios-menu"
+          size={30} />
+          };
+        }
+      }
+    );
+
+
+const AppDrawerNavigator = createDrawerNavigator({
+  Dashboard: {
+    screen: DashboardStackNavigator
+  }
+});
+
+
+const AppSwitchNavigator = createSwitchNavigator({
+  Welcome: { screen: WelcomeScreen },
+  Dashboard: { screen: AppDrawerNavigator }
+});
+
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
